fa4ea5dd79e8573b2e5dab2b36cb2bd336de4494
sheetalsh456 <sheetalsh456@gmail.com>
2017-10-17 20:25:03 +0530

    Added solution for Rumour

---
+++ Rumour/solution.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+double level(double x)
+{
+    return floor(log(x)/log(2));
+}
+int main() {
+    long  long int q;
+    double i,j;
+    double a,b;
+    cin>>q;
+    while(q--)
+    {
+        
+        
+        cin>>a>>b;
+        if(level(a)>level(b))
+        {
+            i = a; j =b;
+            while(level(i)!=level(b))
+            i=floor(i/2);
+            while(i!=j)
+            {
+                i = floor(i/2);
+                j = floor(j/2);
+            }
+            cout<<level(a)+level(b)-2*level(i)<<"\n";
+        }
+        else
+        {
+            i = b; j=a;
+            while(level(i)!=level(a))
+            i=floor(i/2);
+            while(i!=j)
+            {
+                i = floor(i/2);
+                j = floor(j/2);
+               
+            }
+            cout<<level(a)+level(b)-2*level(i)<<"\n";
+            
+        }
+        
+    }
+    return 0;
+}
